aaee6166f6702d7da0e53f2d671d024856e87382
ezhove43 <yuriyes43@gmail.com>
2019-12-13 23:20:51 +0900

    add style templates

---
+++ resources/lang/en/home.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+        Home page Language Lines
+    */
+
+    'publish_data_offer' => 'Publish your data offer',
+    'more' => 'More',    
+    'account' => 'Account',
+    'discover_our_matchmaking_service' => 'Discover our matchmaking service',
+    'explore_use_cases' => 'Explore use cases',
+    'help_customer_care' => 'Help & customer care',
+    'about_databroker_dao' => 'About databroker dao',
+    'partners' => 'Partners',
+    'updates' => 'Updates',
+    'press_media' => 'Press & media',
+    'contact' => 'Contact',
+    /*action*/
+    'mark_spencer' => 'Mark Spencer',
+    'profile_account_info' => 'Profile & Account Info',
+    'wallet' => 'Wallet',
+    'me_buyer' => 'Me as a buyer',
+    'bids' => 'Bids',
+    'purchases' => 'Purchases',
+    'me_seller' => 'Me as a seller',
+    'company_profile' => 'Company Profile',
+    'data_offers' => 'Data Offers',
+    'signout' => 'Sign Out'
+];
