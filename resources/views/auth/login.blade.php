2c9724a7b15d06fd3aebea7f1edd97dd1695d1df
laraman <superman1987417@outlook.com>
2019-12-18 07:39:19 -0800

    -- Update SignIn & Register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,70 +3,55 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-4" id="login_section">
+            <h1 class="h1-smaller color-primary text-center" id="login_title">Please sign in</h1>
+            <p class="text-center h4-intro">To send message to the seller</p>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="email" name="email" class="form-control input_data @error('email')  is-invalid @enderror" placeholder=" "  value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <span>Email Address</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="password" name="password" class="form-control input_data @error('password')  is-invalid @enderror" placeholder=" "  value="{{ old('password') }}" required autocomplete="password" autofocus>
+                    <span>{{ __('Password') }}</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <div class="form-group row mb-0">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn customize-btn pure-material-button-outlined">CONTINUE</button>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" id="forgot_pass_link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Password?') }}
+                            </a>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <hr>
+                <p class="h4-intro">No account yet?</p>
+                <p class="paragraph-small">Let's get you set up on Databroker dao first! It's free!</p>
+                <a class="btn btn-secondary" id="register_link" href="{{ route('register') }}">
+                    {{ __('REGISTER NOW') }}
+                </a>
+
+            </form>
+            
         </div>
     </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,74 +3,136 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+        <div class="col-md-6" id="register_section">
+            <h1 class="h1-smaller color-primary" id="register_title">Let's get you set up on Databroker dao!</h1>
+            <br>            
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="firstname" name="firstname" class="form-control input_data @error('firstname')  is-invalid @enderror" placeholder=" "  value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
+                    <span>First name</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('firstname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="lastname" name="lastname" class="form-control input_data @error('lastname')  is-invalid @enderror" placeholder=" "  value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
+                    <span>Last name</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('lastname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="email" name="email" class="form-control input_data @error('email')  is-invalid @enderror" placeholder=" "  value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <span>Email Address</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="company" name="company" class="form-control input_data @error('company')  is-invalid @enderror" placeholder=" "  value="{{ old('company') }}" required autocomplete="company" autofocus>
+                    <span>Company</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('company')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
+
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="country" name="country" class="form-control input_data @error('country')  is-invalid @enderror" placeholder=" "  value="{{ old('country') }}" required autocomplete="country" autofocus>
+                    <span>Country</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('country')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
+
+                <div class="dropdown-container">
+                    <div class="dropdown" tabindex="1">
+                        <div class="select">
+                            <span>What business are you in?</span>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        <input type="hidden" id="businessName2" name="businessName2" value="Agriculture/Mining/Forestry">
+                        <ul class="dropdown-menu" style="display: none;">
+                            <li value="Agriculture/Mining/Forestry">Agriculture/Mining/Forestry</li>
+                            <li value="Advertising/Media/Publishing">Advertising/Media/Publishing</li>
+                            <li value="Automotive">Automotive</li>
+                            <li value="Construction/Engineering/Infrstructure">Construction/Engineering/Infrstructure</li>
+                            <li value="Chemicals">Chemicals</li>
+                            <li value="Creative">Creative</li>
+                            <li value="Education">Education</li>
+                            <li value="Energy/Utilities">Energy/Utilities</li>
+                            <li value="Financial services &amp; insurance">Financial services &amp; insurance</li>
+                            <li value="Government/Non-profit">Government/Non-profit</li>
+                            <li value="Healthcare">Healthcare</li>
+                            <li value="Hospitality/Tourism">Hospitality/Tourism</li>
+                            <li value="IT services">IT services</li>
+                            <li value="Manufacturing">Manufacturing</li>
+                            <li value="Pharmaceutical/Biotech">Pharmaceutical/Biotech</li>
+                            <li value="Retail/Consumer goods">Retail/Consumer goods</li>
+                            <li value="Telecommunications/Electronics">Telecommunications/Electronics</li>
+                            <li value="Transportation/Logistics">Transportation/Logistics</li>
+                            <li value="">Other industry</li>
+                        </ul>
+                    </div>
+                    <div class="error_notice"> This field is required</div>
                 </div>
-            </div>
+
+                <div class="dropdown-container">
+                    <div class="dropdown" tabindex="1">
+                        <div class="select">
+                            <span>What kind of role do you have?</span>
+                        </div>
+                        <input type="hidden" id="jobTitle2" name="jobTitle2" value="Technical">
+                        <ul class="dropdown-menu" style="display: none;">
+                            <li value="Business">Business</li>
+                            <li value="Technical">Technical</li>
+                            <li value="">Other</li>
+                        </ul>
+                    </div>
+                    <div class="error_notice"> This field is required</div>
+                </div>
+
+                <label class="pure-material-textfield-outlined">
+                    <input type="text" id="password" name="password" class="form-control input_data @error('password')  is-invalid @enderror" placeholder=" "  value="{{ old('password') }}" required autocomplete="password" autofocus>
+                    <span>{{ __('Password') }}</span>
+                    <div class="error_notice"> This field is required</div>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </label>
+                <div class="form-check">
+                    <label class="form-check-label">                
+                        <input class="form-check-input" type="checkbox" value="">
+                        <b class="">Yes, I accept the <font style="color: #78E6D0;">terms and conditions</font> and <font style="color: #78E6D0;">the privacy</font> of Databroker dao</b>
+                        <span class="form-check-sign">
+                            <span class="custom-check check"></span>
+                        </span>
+                    </label>
+                </div>                        
+
+                <button type="submit" class="btn customize-btn pure-material-button-outlined">CREAT ACCOUNT</button>
+                
+            </form>            
         </div>
     </div>
 </div>
