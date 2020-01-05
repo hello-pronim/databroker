0bfe13830b8183be3e6e761d04f1244a9f174b8b
smart_developer88 <smart.developer88@protonmail.com>
2020-01-04 21:56:17 -0800

    merged

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware(['auth','verified']);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function details(Request $request)
+    {        
+        return view('data.details');
+    }
+}
