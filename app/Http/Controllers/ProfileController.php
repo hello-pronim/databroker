934d4f3d72928928afca3b44841aa5cbae3763c0
Anton Yefimov <anton.yefimov@outlook.com>
2020-01-04 21:56:17 -0800

    merged

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
@@ -23,6 +24,11 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        return view('profile');
+        return view('account.profile');
+    }
+
+    public function purchases(Request $request) 
+    {
+        return view('account.purchases');
     }
 }
