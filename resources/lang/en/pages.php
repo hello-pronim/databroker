f3bf330300d116f0974c3b7ff6f1a540623ccea3
Anton Yefimov <anton.yefimov@outlook.com>
2020-01-06 06:08:32 -0800

    complete #43, 48, and 51 pages offers step 2, 3, and step 4.

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -10,6 +10,24 @@ return [
     'format' => 'Format',
     'access_to_data' => 'Access to this data',
     'please_select' => 'Please Select',
-    'enter_url' => 'Enter URL'
+    'enter_url' => 'Enter URL',
     
+    'images' => 'Images',
+    'files' => 'Files',
+    'next' => 'Next',
+    'optional_but_recommended' => 'Optional but recommended',
+    'dummy_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.',
+    'not_sure_what_to_enter' => 'Not sure what to enter?',
+    'view_an_example_pdf' => 'View an example (pdf)',
+    'your_message' => 'Your message',
+
+    'data_offer_step_2' => 'Step 2 of 4: Description of the data',
+    'data_offer_step_3' => 'Step 3 of 4: Use cases',
+    'data_offer_step_4' => 'Step 4 of 4: Samples',
+    'data_offer_step_2_description' => 'Describe the data more in detail. What extra info can help potential buyers ...',
+    'data_offer_step_3_description' => 'What are some typical use cases for your data? What business challenges do',
+    'data_offer_image_upload_description1' => 'Helptext about logo format (max x images) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. ',
+    'data_offer_image_upload_description2' => 'Or browse our image database and pick one',
+    'previous_step' => 'Previous step',
+    'publish_on_marketplace' => 'PUBLISH ON MARKETPLACE',
 ];
