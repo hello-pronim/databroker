74a7bfb579ea3c1b389e1ba2f893ab2032f03bef
ezhove43 <yuriyes43@gmail.com>
2020-01-27 11:40:23 +0900

    Add dynamic user name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
   	        		<li class="nav-item dropdown">
   	        			<a class="nav-link dropdown-toggle" id="account_dropdown" data-toggle="dropdown" aaria-haspopup="true" aria-expanded="false"><i class="icon material-icons mdl-badge" data-badge="!">person</i>{{ trans('home.account') }}</a>
   	        			<div class="dropdown-menu" aria-labelledby="account_dropdown">
-  	        				<h4> {{ trans('home.mark_spencer') }} </h4>
+  	        				<h4> {{ Auth::user()->firstname. ' '. Auth::user()->lastname }} </h4>
   	        				<a class="dropdown-item" href="{{ route('account.profile') }}"> {{ trans('home.profile_account_info') }} </a>
   	        				<!-- <a class="dropdown-item" href="{{ route('account.wallet') }}"> {{ trans('home.wallet') }} </a> -->
   	        				<div class="dropdown-divider"></div>
