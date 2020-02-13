966ae69aae0512fd2012b07186857491821a94d0
peterpan <peterpan120@yandex.com>
2020-02-13 12:10:40 +1000

    privacy policy page

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -15,5 +15,6 @@ return [
 
     'terms_conditions'=>'Terms and conditions',
     'privacy_policy'=>'Privacy policy',
-    'cookie_policy'=>'Cookie policy'
+    'cookie_policy'=>'Cookie policy',
+    'who_we_are'=>'Who we are'
 ];
--- resources/views/about/cookie_policy.blade.php
+++ resources/views/about/cookie_policy.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')       
-<div class="container-fluid app-wapper bg-pattern-side">
+<div class="container-fluid app-wapper bg-pattern-side app-wapper-cookie-policy">
     <div class="container">
-        <div class="row justify-content-center mt-30">
-            <div class="col-md-8">
-            	<h1 class="text-primary text-center text-bold">{{ trans('about.cookie_policy') }}</h1>
+        <div class="mt-30">
+            <h1 class="text-primary text-center text-bold">{{ trans('about.cookie_policy') }}</h1>
+            <div class="row">
+            	<div class="col-md-12">
+            	</div>
             </div>
         </div>
     </div>
