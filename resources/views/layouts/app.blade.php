5c657d1404798281a1dd3df33b1f060dc3ec909c
peterpan <peterpan120@yandex.com>
2020-02-21 09:31:25 +1000

    terms and conditions page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                     <a class="dropdown-item" href="{{ route('help.overview') }}"> {{ trans('home.helpsupport') }} </a>
                     <a class="dropdown-item" href="javascript:;"> {{ trans('home.news') }} </a>
                     <a class="dropdown-item" href="{{ route('about.media_center') }}"> {{ trans('home.media_center') }} </a>
-  	        				<a class="dropdown-item" href="javascript:;"> {{ trans('home.contact_us') }} </a>
+  	        				<a class="dropdown-item" href="{{ route('contact')}}"> {{ trans('home.contact_us') }} </a>
   	        			</div>
   	        		</li>
   	        	</ul>	
