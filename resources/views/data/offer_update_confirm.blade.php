0897de0b39c5645ed74a5d8080f122dc016618db
Anton Yefimov <anton.yefimov@outlook.com>
2020-02-25 04:50:21 -0800

    Add update offer confirmation page instead of publish confirmation page.

---
+++ resources/views/data/offer_update_confirm.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('additional_css')
+@endsection
+
+@section('content')
+<div class="container-fluid app-wapper data-offer confirm mgh25">
+	<div class="bg-half-watermark-right-left">
+		<div class="bg-half-watermark w-right"></div>
+		<div class="bg-half-watermark w-left"></div>
+	</div>
+    <div class="container flex-vcenter">	
+		<div class="row">
+			<div class="col-lg-2"></div>
+			<div class="col-lg-8">
+				<div class="blog-header">
+		            <div class="h1-big text-center color-blue">Your data offer {{$title}} has been updated.</div>
+		        </div>		        
+		        <div class="blog-content">
+		            <div class="h2">You can access it via the <a href="{{route('data_offers_overview')}}">My data offers & products</a> section of your account and edit it any time.</div>
+		            <p class="para"><b>IMPORTANT</b>: To be able to sell or share data related to this data offer, you need to create specific data products. You can do this now, or at any time, via the <a href="{{route('data_offers_overview')}}">My data offers and products</a> section of your account.</p>
+		            <div class="flex-center mgt30">
+		            	<a href="{{$link_to_market}}"><button class="primary-btn mgr30">VIEW ON THE MARKETPLACE</button></a>
+		            	<a href="{{route('data_offer_add_product', ['id'=>$offerId])}}"><button class="secondary-btn">ADD DATA PRODUCTS NOW</button></a>
+		            </div>
+		        </div>
+			</div>
+		</div>
+	</div>   	
+</div>
+
+@endsection
+
+@section('additional_javascript')
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,15 @@ Route::group(['middleware' => ['ReturnAfterAuthentication']], function(){
 
 		Route::get('/data/offers/overview', 'DataController@offers_overview')->name('data_offers_overview');			
 		Route::get('/data/offers/{id}', 'DataController@offer_detail')->where('id', '[0-9]+')->name('data_offer_detail');
+		Route::get('/data/offers/{id}/confirmation', 'DataController@offer_publish_confirm')->where('id', '[0-9]+')->name('data_offer_publish_confirm');
 		Route::get('/data/offers/{id}/edit', 'DataController@offer_edit')->where('id', '[0-9]+')->name('data_offer_edit');
 		Route::post('/data/offers/{id}/update', 'DataController@update_offer')->where('id', '[0-9]+')->name('data.update_offer');
+        Route::get('/data/offers/{id}/confirm-update', 'DataController@offer_update_confirm')->where('id', '[0-9]+')->name('data_offer_update_confirm');
+        		
 		Route::get('/data/offers/{id}/product/add', 'DataController@offer_add_product')->where('id', '[0-9]+')->name('data_offer_add_product');
 		Route::get('/data/offers/{id}/product/{pid}/edit', 'DataController@offer_edit_product')->where('id', '[0-9]+')->where('pid', '[0-9]+')->name('data_offer_edit_product');
 		Route::post('/data/product/add', 'DataController@offer_submit_product')->name('data_offer_submit_product');
 		Route::get('/data/offers/{id}/product/confirmation', 'DataController@offer_product_publish_confirm')->where('id', '[0-9]+')->name('data_offer_product_publish_confirm');
-		Route::get('/data/offers/{id}/confirmation', 'DataController@offer_publish_confirm')->where('id', '[0-9]+')->name('data_offer_publish_confirm');
 		Route::get('/data/send_bid', 'DataController@send_bid')->name('data.send_bid');
 		
 		Route::post('/data/add', 'DataController@add_offer')->name('data.add_offer');
