fbcd8bb694c3f39b12c27be0a1627384012f8827
maplevlad <success.maple@gmail.com>
2020-03-05 14:24:13 +0800

    Top-nav Bar responsive in mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,52 +42,56 @@
     <body>
         <nav class="fixed_bg">
         	<div class="navbar navbar-expand-lg app-top-nav-bar-container">
-        		<div class="container">        		
-  	    			<ul class="navbar-nav left">
-  	        		<li class="nav-item"><a class="nav-link" href="{{ route('data_offer_publish') }}">{{ trans('home.publish_data_offer') }}</a></li>
-  	        		<li class="nav-item dropdown">
-  	        			<a class="nav-link dropdown-toggle" id="more_dropdown" data-toggle="dropdown" aaria-haspopup="true" aria-expanded="false">{{ trans('home.more') }}</a>
-  	        			<div class="dropdown-menu" aria-labelledby="more_dropdown">
-  	        				<a class="dropdown-item" href="{{ route('about.about') }}"> {{ trans('home.about_databroker') }} </a>
-                    <a class="dropdown-item" href="{{ route('about.partners') }}"> {{ trans('home.partners') }} </a>
-                    <a class="dropdown-item" href="{{ route('about.matchmaking') }}"> {{ trans('home.datamatch') }} </a>
-                    <a class="dropdown-item" href="{{ route('about.usecase') }}"> {{ trans('home.usecase') }} </a>
-                    <a class="dropdown-item" href="{{ route('help.overview') }}"> {{ trans('home.helpsupport') }} </a>
-                    <a class="dropdown-item" href="javascript:;"> {{ trans('home.news') }} </a>
-                    <a class="dropdown-item" href="{{ route('about.media_center') }}"> {{ trans('home.media_center') }} </a>
-  	        				<a class="dropdown-item" href="{{ route('contact')}}"> {{ trans('home.contact_us') }} </a>
-  	        			</div>
-  	        		</li>
-  	        	</ul>	
-              @if( Auth::user() )
-  	        	<ul class="navbar-nav right">			        		
-  	        		<li class="nav-item dropdown">
-  	        			<a class="nav-link dropdown-toggle" id="account_dropdown" data-toggle="dropdown" aaria-haspopup="true" aria-expanded="false"><i class="icon material-icons mdl-badge">person</i><!-- <i class="icon material-icons mdl-badge" data-badge="!">person</i> -->{{ trans('home.account') }}</a>
-  	        			<div class="dropdown-menu" aria-labelledby="account_dropdown">                                        
-  	        				<h4> {{ Auth::user()->firstname. ' '. Auth::user()->lastname }} </h4>                    
-  	        				<a class="dropdown-item" href="{{ route('account.profile') }}"> {{ trans('home.profile_account_info') }} </a>
-  	        				<!-- <a class="dropdown-item" href="{{ route('account.wallet') }}"> {{ trans('home.wallet') }} </a> -->
-  	        				<div class="dropdown-divider"></div>
-  	        				<h5> {{ trans('home.me_buyer') }} </h5>
-  	        				<a class="dropdown-item" href="{{ route('profile.bids') }}"> {{ trans('home.bids') }} </a>
-  	        				<a class="dropdown-item" href="{{ route('account.purchases') }}"> {{ trans('home.purchases') }} </a>
-  	        				<div class="dropdown-divider"></div>
-  	        				<h5> {{ trans('home.me_seller') }} </h5>
-  	        				<a class="dropdown-item" href="{{ route('account.company') }}"> {{ trans('home.company_profile') }} </a>
-  	        				<a class="dropdown-item" href="{{ route('data_offers_overview' )}}"> {{ trans('home.data_offers') }} </a>
-  	        				<div class="dropdown-divider"></div>
-  	        				<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();"> {{ trans('home.signout') }} </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-  	        			</div>
-  	        		</li>
-  	        	</ul>	
-              @else
-                <div class="right"><a href="{{ route('login') }}">{{ trans('auth.login') }}</a> or <a href="{{ route('register') }}">{{ trans('auth.register') }}</a>
-                </div>  
-              @endif
+        		<div class="container">
+                <div class="col-md-10 col-sm-12">
+                    <ul class="navbar-nav left">
+                      <li class="nav-item" id="top-nav-item"><a class="nav-link" href="{{ route('data_offer_publish') }}">{{ trans('home.publish_data_offer') }}</a></li>
+                      <li class="nav-item dropdown" id="top-nav-item-dropdown">
+                        <a class="nav-link dropdown-toggle" id="more_dropdown" data-toggle="dropdown" aaria-haspopup="true" aria-expanded="false">{{ trans('home.more') }}</a>
+                        <div class="dropdown-menu" aria-labelledby="more_dropdown">
+                          <a class="dropdown-item" href="{{ route('about.about') }}"> {{ trans('home.about_databroker') }} </a>
+                          <a class="dropdown-item" href="{{ route('about.partners') }}"> {{ trans('home.partners') }} </a>
+                          <a class="dropdown-item" href="{{ route('about.matchmaking') }}"> {{ trans('home.datamatch') }} </a>
+                          <a class="dropdown-item" href="{{ route('about.usecase') }}"> {{ trans('home.usecase') }} </a>
+                          <a class="dropdown-item" href="{{ route('help.overview') }}"> {{ trans('home.helpsupport') }} </a>
+                          <a class="dropdown-item" href="javascript:;"> {{ trans('home.news') }} </a>
+                          <a class="dropdown-item" href="{{ route('about.media_center') }}"> {{ trans('home.media_center') }} </a>
+                          <a class="dropdown-item" href="{{ route('contact')}}"> {{ trans('home.contact_us') }} </a>
+                        </div>
+                      </li>
+                    </ul>	
+                </div>
+                <div class="col-md-2 col-sm-12">
+                      @if( Auth::user() )
+                      <ul class="navbar-nav right">			        		
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" id="account_dropdown" data-toggle="dropdown" aaria-haspopup="true" aria-expanded="false"><i class="icon material-icons mdl-badge">person</i><!-- <i class="icon material-icons mdl-badge" data-badge="!">person</i> -->{{ trans('home.account') }}</a>
+                          <div class="dropdown-menu" aria-labelledby="account_dropdown">                                        
+                            <h4> {{ Auth::user()->firstname. ' '. Auth::user()->lastname }} </h4>                    
+                            <a class="dropdown-item" href="{{ route('account.profile') }}"> {{ trans('home.profile_account_info') }} </a>
+                            <!-- <a class="dropdown-item" href="{{ route('account.wallet') }}"> {{ trans('home.wallet') }} </a> -->
+                            <div class="dropdown-divider"></div>
+                            <h5> {{ trans('home.me_buyer') }} </h5>
+                            <a class="dropdown-item" href="{{ route('profile.bids') }}"> {{ trans('home.bids') }} </a>
+                            <a class="dropdown-item" href="{{ route('account.purchases') }}"> {{ trans('home.purchases') }} </a>
+                            <div class="dropdown-divider"></div>
+                            <h5> {{ trans('home.me_seller') }} </h5>
+                            <a class="dropdown-item" href="{{ route('account.company') }}"> {{ trans('home.company_profile') }} </a>
+                            <a class="dropdown-item" href="{{ route('data_offers_overview' )}}"> {{ trans('home.data_offers') }} </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"> {{ trans('home.signout') }} </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                          </div>
+                        </li>
+                      </ul>	
+                      @else
+                        <div class="right" id="top-nav-right"><a href="{{ route('login') }}">{{ trans('auth.login') }}</a> or <a href="{{ route('register') }}">{{ trans('auth.register') }}</a>
+                        </div>  
+                      @endif
+                </div>
 	        	</div>     	
         	</div>        	   	
         </nav>
