d749ce5186165425cdfa9ebefbca21228492450b
maplevlad <success.maple@gmail.com>
2020-03-25 12:07:22 +0800

    query fix in article table.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -90,11 +90,23 @@
                 <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
                     <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
                         <li class="m-menu__item  m-menu__item--parent"  aria-haspopup="true">
-                            <a href="{{ route('admin.usecases') }}" class="m-menu__link" id="usecase-sidebar">
-                                <i class="m-menu__link-icon"></i>
-                                        Use cases
-                                <i class="m-menu__ver-arrow la la-angle-right"></i>
-                            </a>
+                            <!-- <a href="{{ route('admin.usecases') }}" class="m-menu__link" id="usecase-sidebar"> -->
+                                <span class="m-menu__link" id="usecase-sidebar">
+                                    <i class="m-menu__link-icon"></i>
+                                            Use cases
+                                    <i class="m-menu__ver-arrow la la-angle-right"></i>
+                                </span>
+                                
+                            <!-- </a> -->
+                            <ul class="subsider_ul">
+                                <li class="m-menu__sidebar"  aria-haspopup="true"><a href="{{ route('admin.usecases') }}" class="m-menu__link">Transport</a></li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">People</li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">Environment</li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">Agriculture</li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">Energy</li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">Economy</li>
+                                <li class="m-menu__sidebar"  aria-haspopup="true">Supply Chain</li>
+                            </ul>
                         </li>
                         <li class="m-menu__item  m-menu__item--parent"  aria-haspopup="true">
                             <a href="{{ route('admin.updates') }}" class="m-menu__link" id="usecase-sidebar">
