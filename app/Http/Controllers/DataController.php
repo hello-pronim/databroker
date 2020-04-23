d71205bf9063713d425266aeee960ff46d4eac00
Andrei Iunda <peterpan120@yandex.com>
2020-04-24 03:13:57 +1000

    offer edit error

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -198,7 +198,7 @@ class DataController extends Controller
         }
 
         $usecase = UseCase::where('offerIdx', $offerId)->first();
-
+.
         //$offer_path = URL::to('/uploads/offer');
         
         $offer_images = [$offer['offerImage']];
@@ -255,8 +255,10 @@ class DataController extends Controller
             $gallery_map[$content][$subcontent][$sequence] = ['id' => $id, 'community'=>$content, 'communityName'=>$communityName, 'url' => $path, 'thumb' => $thumb];
 
         }
-
-        $data = array( 'offerIdx', 'regions', 'countries', 'communities', 'offer', 'products', 'id', 'link_to_market', 'regionCheckList', 'usecase', 'sample_files', 'sample_images', 'offersample_path', 'offer_path', 'offer_images', 'theme_json', 'themeCheckList', 'gallery_map' );
+        if(isset($offer_path))
+            $data = array( 'offerIdx', 'regions', 'countries', 'communities', 'offer', 'products', 'id', 'link_to_market', 'regionCheckList', 'usecase', 'sample_files', 'sample_images', 'offersample_path', 'offer_path', 'offer_images', 'theme_json', 'themeCheckList', 'gallery_map' );
+        else
+            $data = array( 'offerIdx', 'regions', 'countries', 'communities', 'offer', 'products', 'id', 'link_to_market', 'regionCheckList', 'usecase', 'sample_files', 'sample_images', 'offersample_path', 'offer_images', 'theme_json', 'themeCheckList', 'gallery_map' );
         // die(json_encode(compact($data)));
         return view('data.offers', compact($data));
     }
