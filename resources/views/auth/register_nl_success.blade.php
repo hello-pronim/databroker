0fc6c38be26e1833b8e6ce4aa0e1724d1a5458ca
Andrei Iunda <peterpan120@yandex.com>
2020-06-09 23:24:25 +1000

    redirect to home page if click continue button in newsletter signup confirm page

--- resources/views/auth/register_nl_success.blade.php
+++ resources/views/auth/register_nl_success.blade.php
@@ -9,7 +9,9 @@
 					<h1 class="text-primary text-center text-bold">You successfully signed up for our NewsBytes.</h1>
                 	<h4 class="text-bold">The latest Databroker updates delivered straight in your inbox!</h4>
 					<div class="buttons flex-vcenter">
-	                    <a href="{{$next_url}}"><button type="button" class="customize-btn btn-next">Continue</button></a>
+	                    <a href="{{(strpos($next_url, 'register_nl') !== false || !isset($next_url))? route('home'): $next_url}}">
+	                    	<button type="button" class="customize-btn btn-next">Continue</button>
+	                    </a>
 	                </div>
 				</div>
 			</div>
